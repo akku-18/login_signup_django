fffce061736491347faafa54944e359f3711990d
akku-18 <akshanshgarg18@gmail.com>
2022-10-03 23:13:31 +0530

    final commit

---
+++ authentication/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+# i am in, u r hecked
+#gaand fat gayi
---
+++ authentication/urls.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from django.urls import path, include
+from django.contrib import admin
+from . import views
+
+urlpatterns=[
+    path('', views.home , name="home"),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name ='signout'),
+]
